8843f4700e7f1c3d5963fb0e66acaa6aab2c0a93
Martin Nguyen <nguymart@os1.engr.oregonstate.edu>
2018-11-09 14:18:57 -0800

    skeleton stuff

---
+++ input.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
+
+
+int main()
+{
+	getInput()
+	return 0;
+}
